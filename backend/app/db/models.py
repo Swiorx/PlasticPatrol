dfbadda26582760b70d6299ec33e45c045071f72
Calin Murariu <swiorx@gmail.com>
2026-04-24 21:29:01 +0300

    added more parameters to the database

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -9,7 +9,11 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    is_active = Column(Boolean, default=True)
+    hashed_password = Column(String, nullable=False)
     eco_points = Column(Integer, default=0)
+    
 
 class PlasticDebris(Base):
     __tablename__ = "plastic_debris"
@@ -18,6 +22,9 @@ class PlasticDebris(Base):
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     size_category = Column(String, default="small") # ex: small, medium, large
-    detected_at = Column(DateTime, default=datetime.utcnow)
+    detected_at = Column(DateTime, default=datetime.now(datetime.timezone.utc))
     is_collected = Column(Boolean, default=False)
     collected_by = Column(Integer, default=None) # ID-ul userului care a colectat
+    collected_at = Column(DateTime, default=None) # Data colectării
+    eco_points = Column(Integer, default=0) # Punctele care ar trebui acordate pentru colectare
+    
